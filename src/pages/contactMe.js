4f82bdbe7781fcf3362c4409cb191745d7b4d1b9
Gabriel Aldea D <gabriel.aldea@gmail.com>
2023-06-02 13:07:54 -0400

    working on the ContactMe styling

--- src/pages/contactMe.js
+++ src/pages/contactMe.js
@@ -1,6 +1,7 @@
 import '../styles/contactMe.css';
 import React from 'react';
-import { Button, Form } from 'react-bootstrap';
+import { Button, Col, Container, Form, Image, Row } from 'react-bootstrap';
+import Profile from '../img/photos/profile.jpg';
 
 const ContactMe = () => {
   const [name, setName] = React.useState('');
@@ -38,36 +39,58 @@ const ContactMe = () => {
   }
 
   return (
-    <Form className="contact-form" name="contact" onSubmit={handleSubmit}>
-      <h2>Let's talk</h2>
-      <Form.Group controlId="name">
-        <Form.Label>Name</Form.Label>
-        <Form.Control
-          type="text"
-          name="name"
-          onChange={(e) => setName(e.target.value)}
-        />
-      </Form.Group>
-      <Form.Group controlId="email">
-        <Form.Label>Email</Form.Label>
-        <Form.Control
-          type="email"
-          name="email"
-          onChange={(e) => setEmail(e.target.value)}
-        />
-      </Form.Group>
-      <Form.Group controlId="message">
-        <Form.Label>Message</Form.Label>
-        <Form.Control
-          as="textarea"
-          name="message"
-          onChange={(e) => setMessage(e.target.value)}
-        />
-      </Form.Group>
-      <Button variant="primary" type="submit">
-        Submit
-      </Button>
-    </Form>
+    <Container>
+      <Row>
+        <Col md={6}>
+          <Form
+            className="contact-form my-4"
+            name="contact"
+            onSubmit={handleSubmit}
+          >
+            <h2>Let's talk</h2>
+            <p>
+              Interested in a framed picture? Have some project ideas? Send me a
+              message!
+            </p>
+            <Form.Group className="mt-4" controlId="name">
+              <Form.Label>Name</Form.Label>
+              <Form.Control
+                type="text"
+                name="name"
+                onChange={(e) => setName(e.target.value)}
+              />
+            </Form.Group>
+            <Form.Group className="mt-4" controlId="email">
+              <Form.Label>Email</Form.Label>
+              <Form.Control
+                type="email"
+                name="email"
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </Form.Group>
+            <Form.Group className="mt-4" controlId="message">
+              <Form.Label>Message</Form.Label>
+              <Form.Control
+                as="textarea"
+                name="message"
+                onChange={(e) => setMessage(e.target.value)}
+              />
+            </Form.Group>
+            <Button className="mt-4" type="submit">
+              Submit
+            </Button>
+          </Form>
+        </Col>
+        <Col md={6}>
+          <Image
+            className="rounded-circle mt-5"
+            src={Profile}
+            alt="profilepic"
+            fluid
+          />
+        </Col>
+      </Row>
+    </Container>
   );
 };
 
