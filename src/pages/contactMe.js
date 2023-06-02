21a6c83503fe69e4dd063badceb968c99e9cc92d
Gabriel Aldea D <gabriel.aldea@gmail.com>
2023-06-02 12:39:55 -0400

    working on styling, making sure form is still working

--- src/pages/contactMe.js
+++ src/pages/contactMe.js
@@ -1,4 +1,6 @@
+import '../styles/contactMe.css';
 import React from 'react';
+import { Button, Form } from 'react-bootstrap';
 
 const ContactMe = () => {
   const [name, setName] = React.useState('');
@@ -36,58 +38,36 @@ const ContactMe = () => {
   }
 
   return (
-    <form
-      netlify
-      name="contact"
-      onSubmit={handleSubmit}
-      className="lg:w-1/3 md:w-1/2 flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0"
-    >
-      <h2 className="sm:text-4xl text-3xl mb-1 font-medium title-font">
-        Let's talk
-      </h2>
-      <p className="leading-relaxed mb-5">Name</p>
-      <div className="relative mb-4">
-        <label
-          htmlFor="name"
-          className="leading-7 text-sm text-gray-400"
-        ></label>
-        <input
+    <Form className="contact-form" name="contact" onSubmit={handleSubmit}>
+      <h2>Let's talk</h2>
+      <Form.Group controlId="name">
+        <Form.Label>Name</Form.Label>
+        <Form.Control
           type="text"
-          id="name"
           name="name"
-          className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
           onChange={(e) => setName(e.target.value)}
         />
-      </div>
-      <div className="relative mb-4">
-        <label htmlFor="email" className="leading-7 text-sm text-gray-400">
-          Email
-        </label>
-        <input
+      </Form.Group>
+      <Form.Group controlId="email">
+        <Form.Label>Email</Form.Label>
+        <Form.Control
           type="email"
-          id="email"
           name="email"
-          className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
           onChange={(e) => setEmail(e.target.value)}
         />
-      </div>
-      <div className="relative mb-4">
-        <label htmlFor="message" className="leading-7 text-sm text-gray-400">
-          {' '}
-          Message{' '}
-        </label>
-        <textarea
-          id="message"
+      </Form.Group>
+      <Form.Group controlId="message">
+        <Form.Label>Message</Form.Label>
+        <Form.Control
+          as="textarea"
           name="message"
-          className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 h-32 text-base outline-none text-gray-100 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
           onChange={(e) => setMessage(e.target.value)}
         />
-      </div>
-      <button
-        type="submit"
-        className="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg"
-      ></button>
-    </form>
+      </Form.Group>
+      <Button variant="primary" type="submit">
+        Submit
+      </Button>
+    </Form>
   );
 };
 
